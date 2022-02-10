8ba5e295c2919fc22bc407b70c66da020b14ab9f
Javier Manobanda <51manobandajavier@gmail.com>
2022-02-10 17:00:31 -0500

    :bug: fix bug in parameter schedule.
    pass the correct parameter in the class

--- schedule/models/employee.py
+++ schedule/models/employee.py
@@ -4,7 +4,7 @@ from schedule.models.schedule import Schedule
 class Employee:
     def __init__(self, name: str, schedule: Schedule = None):
         self.name = name
-        self.schedule = Schedule
+        self.schedule = schedule
 
     def addSchedule(self, schedule: Schedule):
         self.schedule = schedule
